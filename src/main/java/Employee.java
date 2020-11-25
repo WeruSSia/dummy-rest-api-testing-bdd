30fe115570c75c4e8f2b51e32fe490de9ff2fd78
WeruSSia <werussia16@gmail.com>
2020-11-25 13:42:33 +0100

    basic dtos for Employee, EmployeeData and Employees created

---
+++ src/main/java/Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+    private String status;
+    private EmployeeData data;
+    private String message;
+
+    public Employee() {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public EmployeeData getData() {
+        return data;
+    }
+
+    public void setData(EmployeeData data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/Employees.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Employees {
+    private String status;
+    private List<EmployeeData> data;
+
+    public Employees() {
+
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<EmployeeData> getData() {
+        return data;
+    }
+
+    public void setData(List<EmployeeData> data) {
+        this.data = data;
+    }
+}
