cf7f24b72857d7a59a3d2752c637f1c5896183bc
WeruSSia <werussia16@gmail.com>
2020-11-25 15:41:45 +0100

    dtos for NewEmployee and NewEmployeeData

---
+++ src/main/java/NewEmployee.java
@@ -0,0 +1,32 @@
+public class NewEmployee {
+    private String status;
+    private NewEmployeeData data;
+    private String message;
+
+    public NewEmployee() {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public NewEmployeeData getData() {
+        return data;
+    }
+
+    public void setData(NewEmployeeData data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
