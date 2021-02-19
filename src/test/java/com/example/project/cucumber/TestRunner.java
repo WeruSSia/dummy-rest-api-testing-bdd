ba7417a2415cf937e10e459cf2b03b264532eca1
WeruSSia <werussia16@gmail.com>
2021-02-19 12:58:53 +0100

    cucumber separated

--- src/test/java/com/example/project/cucumber/TestRunner.java
+++ src/test/java/com/example/project/cucumber/TestRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/scenarios"}, plugin = {"pretty"}, glue={"com.example.project.cucumber"})
+@CucumberOptions(features = {"src/test/resources/cucumberScenarios"}, plugin = {"pretty"}, glue={"com.example.project.cucumber"})
 public class TestRunner {
 }
